ba2e1616e47d8afbbbb81872be555c07d5e640e3
Yeji Han <yejihanc@gmail.com>
2021-11-25 16:34:07 +0900

    Fixed list initialization for Student.final_timetable
    Done
    - assigned all Students' with [] final_timetable
    - changed main.py

--- main.py
+++ main.py
@@ -4,23 +4,31 @@ from registration.registration_system import *
 from util.parse import parse_course_file, parse_student_file, generate_course_type_enum
 
 if __name__ == "__main__":
-    courses_dict = parse_course_file("2021 정규학기 과목별 추첨여부", "2021 봄학기 과목.xls", Semester.SPRING)
-    students_dict = parse_student_file("2021 정규학기 수강신청 내역", courses_dict)
+    courses_dict = parse_course_file("2021 정규학기 과목별 추첨여부.xlsx", "2021 봄학기 과목.xls", Semester.SPRING)
+    students_dict = parse_student_file("2021 정규학기 수강신청 내역.xlsx", courses_dict, Semester.SPRING)
     
     students = list(students_dict.values())
 
     lottery_system = LotterySystem(copy.deepcopy(courses_dict))
     result_students = lottery_system.register_students(copy.deepcopy(students))
-
+    
+    print(result_students[:5])
+    
     # Change distributions to see various results
     major_priority_system = MajorPrioritySystem(copy.deepcopy(courses_dict))
     result_students = major_priority_system.register_students(copy.deepcopy(students), (0.3, 0.3, 0.1))
 
+    print(result_students[:5])    
+
     # Change graduate standard and percentage to see various results
     grade_priority_system = GradePrioritySystem(copy.deepcopy(courses_dict))
     result_students = grade_priority_system.register_students(copy.deepcopy(students), 2018, 0.25)
+    
+    print(result_students[:5])
 
     # Change distributions to see various results
     top3_priority_system = Top3PrioritySystem(copy.deepcopy(courses_dict))
-    result_students = top3_priority_system.designate_priority(copy.deepcopy(students), (0.5, 0.4, 0.1))
-    result_students = top3_priority_system.register_students(copy.deepcopy(students), (0.2, 0.2, 0.2))
+    prioritized_students = top3_priority_system.designate_priority(copy.deepcopy(students), (0.5, 0.4, 0.1))
+    result_students = top3_priority_system.register_students(prioritized_students, (0.2, 0.2, 0.2))
+
+    print(result_students[:5])
