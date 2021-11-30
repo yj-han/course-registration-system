ef8915058cffa68b90bfa301e17f36fe52091993
김정은 <47321608+jjungkang2@users.noreply.github.com>
2021-11-30 17:21:14 +0900

    Initial implementation of our own system Done - implemented registration/registration_system/top3_based_1_system.py - applied Top3Based1System to main.py TODO - Write test code to verity system - Visualize and observe if our system is working properly

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import copy
 from registration.course import Semester
+from registration.registration_system import top3_based_1_system
 from registration.registration_system.lottery_system import LotterySystem
 from registration.registration_system.major_priority_system import MajorPrioritySystem
 from registration.registration_system.grade_priority_system import GradePrioritySystem
 from registration.registration_system.top3_priority_system import Top3PrioritySystem
+from registration.registration_system.top3_based_1_system import Top3Based1System
 from util.parse import parse_course_file, parse_student_file
 
 if __name__ == "__main__":
@@ -33,5 +35,12 @@ if __name__ == "__main__":
     top3_priority_system = Top3PrioritySystem(copy.deepcopy(courses_dict))
     prioritized_students = top3_priority_system.designate_priority(copy.deepcopy(students), (0.5, 0.4, 0.1))
     result_students = top3_priority_system.register_students(prioritized_students, (0.2, 0.2, 0.2))
+
+    print(result_students[:5])
+
+    # Change distributions to see various results
+    top3_based_1_system = Top3Based1System(copy.deepcopy(courses_dict))
+    prioritized_students = top3_based_1_system.designate_priority(copy.deepcopy(students), (0.5, 0.4, 0.1))
+    result_students = top3_based_1_system.register_students(prioritized_students, (0.2, 0.2, 0.2), 0.3, 0.3, 2018)
 
     print(result_students[:5])
